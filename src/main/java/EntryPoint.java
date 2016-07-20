a2e172fc5cdc26474c579af2eb8420192678e99c
Team Meerkat <olga.bobrovnytska@bskyb.com>
2016-07-20 15:17:24 +0100

    Omar -  'Embedded Jetty server example' - Servlet with Hello world

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,4 +1,11 @@
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by Omar on 20/07/16.
@@ -8,7 +15,15 @@ public class EntryPoint {
     public static void main(String[] args) throws Exception {
         int port = 8080;
         Server server = new Server(port);
+        server.setHandler(new HelloJettyHandler());
         server.start();
         server.join();
+    }
+
+    private static class HelloJettyHandler extends AbstractHandler {
+        public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
+            request.setHandled(true);
+            httpServletResponse.getWriter().write("Hello embedded jetty!!!");
+        }
     }
 }
