0c17de59297a0e3cf6bfb4086643c2a6b64d0e97
Team Meerkat <olga.bobrovnytska@bskyb.com>
2016-07-20 15:58:31 +0100

    Omar -  'Embedded Jetty server example' - Using servlets

--- src/main/java/EntryPoint.java
+++ src/main/java/EntryPoint.java
@@ -1,9 +1,10 @@
-import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
-import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -19,21 +20,28 @@ public class EntryPoint {
         Server server = new Server();
         server.addConnector(serverConnector(defaultPort, server));
         server.addConnector(serverConnector(adminPort, server));
-        server.setHandler(new HelloJettyHandler());
+        server.setHandler(helloServletHandler());
         server.start();
         server.join();
+    }
+
+    private static ServletContextHandler helloServletHandler() {
+        ServletContextHandler servletHandler = new ServletContextHandler();
+        servletHandler.addServlet(new ServletHolder(new HelloWorldHttpServlet()), "/hello");
+        return servletHandler;
+    }
+
+    private static class HelloWorldHttpServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.getWriter().write("Hello from a servlet!!!!");
+        }
+
     }
 
     private static ServerConnector serverConnector(int port, Server server) {
         ServerConnector connector = new ServerConnector(server);
         connector.setPort(port);
         return connector;
-    }
-
-    private static class HelloJettyHandler extends AbstractHandler {
-        public void handle(String s, Request request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException, ServletException {
-            request.setHandled(true);
-            httpServletResponse.getWriter().write("Hello embedded jetty!!!");
-        }
     }
 }
