740797fec72baa4d118593ee69e9d4def7b67902
Team Mongoose <mongoose@bskyb.com>
2016-07-27 16:40:40 +0100

    Omar - 'Embedded Jetty server example' - Fixed build to be able to access bodar repo. Upgraded config to java 8. Added capturing requests and responses in the test.

--- src/test/java/AcceptanceTest.java
+++ src/test/java/AcceptanceTest.java
@@ -1,7 +1,7 @@
 import com.googlecode.yatspec.junit.SpecRunner;
+import com.googlecode.yatspec.state.givenwhenthen.*;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.junit.After;
@@ -14,10 +14,11 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpecRunner.class)
-public class AcceptanceTest {
+public class AcceptanceTest extends TestState {
 
     private ExampleApp exampleApp = new ExampleApp();
     private HttpResponse response;
+    private String responseBody;
 
     @Before
     public void setUp() throws Exception {
@@ -31,28 +32,34 @@ public class AcceptanceTest {
 
     @Test
     public void shouldReturnHelloWorld() throws Exception {
-        whenWeMakeARequestTo("http://localhost:8080/hello");
+        when(weMakeAGetRequestTo("http://localhost:8080/hello"));
         thenTheResponseCodeIs200AndTheBodyIs("Hello from a servlet!!!!");
     }
 
     @Test
     public void shouldFail() throws Exception {
-        whenWeMakeARequestTo("http://localhost:8080/a/bad/url");
+        when(weMakeAGetRequestTo("http://localhost:8080/a/bad/url"));
         thenItReturnsAStatusCodeOf(404);
+    }
+
+    private ActionUnderTest weMakeAGetRequestTo(String uri) {
+        return (interestingGivens, capturedInputAndOutputs) -> whenWeMakeARequestTo(capturedInputAndOutputs, new HttpGet(uri));
     }
 
     private void thenItReturnsAStatusCodeOf(int expected) {
         assertThat(response.getStatusLine().getStatusCode()).isEqualTo(expected);
     }
 
-    private void whenWeMakeARequestTo(String uri) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-        HttpGet request = new HttpGet(uri);
-        response = httpClient.execute(request);
+    private CapturedInputAndOutputs whenWeMakeARequestTo(CapturedInputAndOutputs capturedInputAndOutputs, HttpGet request) throws IOException {
+        capturedInputAndOutputs.add("Request", request);
+        response = HttpClientBuilder.create().build().execute(request);
+        responseBody = EntityUtils.toString(response.getEntity());
+        capturedInputAndOutputs.add("Response", responseBody);
+        return capturedInputAndOutputs;
     }
 
     private void thenTheResponseCodeIs200AndTheBodyIs(String expected) throws IOException {
         thenItReturnsAStatusCodeOf(200);
-        assertThat(EntityUtils.toString(response.getEntity())).isEqualTo(expected);
+        assertThat(responseBody).isEqualTo(expected);
     }
 }
