32d5d9788ce0199146d863da46631f71d8f22b65
Team Mongoose <mongoose@bskyb.com>
2016-08-03 17:31:08 +0100

    Omar - 'Embedded Jetty server example' - Added automatic generation of Sequence Diagrams

--- src/test/java/AcceptanceTest.java
+++ src/test/java/AcceptanceTest.java
@@ -1,5 +1,14 @@
+import com.googlecode.yatspec.junit.SpecResultListener;
 import com.googlecode.yatspec.junit.SpecRunner;
-import com.googlecode.yatspec.state.givenwhenthen.*;
+import com.googlecode.yatspec.junit.WithCustomResultListeners;
+import com.googlecode.yatspec.plugin.sequencediagram.ByNamingConventionMessageProducer;
+import com.googlecode.yatspec.plugin.sequencediagram.SequenceDiagramGenerator;
+import com.googlecode.yatspec.plugin.sequencediagram.SvgWrapper;
+import com.googlecode.yatspec.rendering.html.DontHighlightRenderer;
+import com.googlecode.yatspec.rendering.html.HtmlResultRenderer;
+import com.googlecode.yatspec.state.givenwhenthen.ActionUnderTest;
+import com.googlecode.yatspec.state.givenwhenthen.CapturedInputAndOutputs;
+import com.googlecode.yatspec.state.givenwhenthen.TestState;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
@@ -11,10 +20,12 @@ import org.junit.runner.RunWith;
 
 import java.io.IOException;
 
+import static java.lang.String.format;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpecRunner.class)
-public class AcceptanceTest extends TestState {
+public class AcceptanceTest extends TestState implements WithCustomResultListeners {
 
     private ExampleApp exampleApp = new ExampleApp();
     private HttpResponse response;
@@ -28,6 +39,18 @@ public class AcceptanceTest extends TestState {
     @After
     public void tearDown() throws Exception {
         exampleApp.stop();
+        capturedInputAndOutputs.add("Sequence Diagram", generateSequenceDiagram());
+    }
+
+    private SvgWrapper generateSequenceDiagram() {
+        return new SequenceDiagramGenerator().generateSequenceDiagram(new ByNamingConventionMessageProducer().messages(capturedInputAndOutputs));
+    }
+
+    @Override
+    public Iterable<SpecResultListener> getResultListeners() throws Exception {
+        return singletonList(new HtmlResultRenderer()
+                .withCustomHeaderContent(SequenceDiagramGenerator.getHeaderContentForModalWindows())
+                .withCustomRenderer(SvgWrapper.class, new DontHighlightRenderer<>()));
     }
 
     @Test
@@ -51,10 +74,10 @@ public class AcceptanceTest extends TestState {
     }
 
     private CapturedInputAndOutputs whenWeMakeARequestTo(CapturedInputAndOutputs capturedInputAndOutputs, HttpGet request) throws IOException {
-        capturedInputAndOutputs.add("Request", request);
+        capturedInputAndOutputs.add(format("Request from %s to %s", "a_user", "helloWorldApp"), request);
         response = HttpClientBuilder.create().build().execute(request);
         responseBody = EntityUtils.toString(response.getEntity());
-        capturedInputAndOutputs.add("Response", responseBody);
+        capturedInputAndOutputs.add(format("Response from %s to %s", "helloWorldApp", "a_user"), response.getStatusLine().toString());
         return capturedInputAndOutputs;
     }
 
