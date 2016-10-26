0ea5892a44b8855a7df66f848fca2ed02381701c
Wojciech Bulaty <wojciech.bulaty@gmail.com>
2016-10-26 14:46:46 +0100

    Omar -  'Acceptance testing with Yatspec' - Improving test readability.

--- src/main/java/servlets/StarWarsCharacterHttpServlet.java
+++ src/main/java/servlets/StarWarsCharacterHttpServlet.java
@@ -20,13 +20,13 @@ public class StarWarsCharacterHttpServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        CloseableHttpResponse lukeResponse = getRequestTo("http://localhost:8888/api/people/1/");
+        CloseableHttpResponse lukeResponse = FireAGetRequestTo("http://localhost:8888/api/people/1/");
         DocumentContext lukeJsonDocument = JsonPath.parse(EntityUtils.toString(lukeResponse.getEntity()));
 
         String lukeName = lukeJsonDocument.read("$.name");
 
-        String lukeSpecies = extractName(getRequestTo(lukeJsonDocument.read("$.species[0]")));
-        String lukeHomeworld = extractName(getRequestTo(lukeJsonDocument.read("$.homeworld")));
+        String lukeSpecies = extractName(FireAGetRequestTo(lukeJsonDocument.read("$.species[0]")));
+        String lukeHomeworld = extractName(FireAGetRequestTo(lukeJsonDocument.read("$.homeworld")));
 
         response.getWriter().print(format("{\"Description\": \"%s is a %s from %s\"}", lukeName, lukeSpecies, lukeHomeworld));
         response.setStatus(200);
@@ -36,7 +36,7 @@ public class StarWarsCharacterHttpServlet extends HttpServlet {
         return JsonPath.parse(EntityUtils.toString(lukeSpeciesResponse.getEntity())).read("$.name");
     }
 
-    private CloseableHttpResponse getRequestTo(String uri) throws IOException {
+    private CloseableHttpResponse FireAGetRequestTo(String uri) throws IOException {
         HttpGet lukeRequest = new HttpGet(uri);
         return HttpClientBuilder.create().build().execute(lukeRequest);
     }
